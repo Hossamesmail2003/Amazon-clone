e1bd3604e2822ef243dc441356fcc87d15dcba78
Hossamesmail2003 <hossam02003@gmail.com>
2024-10-21 21:04:10 +0300

    add brand list , detail api

--- product/api.py
+++ product/api.py
@@ -1,25 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializer import ProductSerializer
-from .models import Product
+from .serializer import ProductSerializer , BrandSerializer
+from .models import Product , Brand
 from rest_framework import generics
 
 
 
-@api_view(['GET'])
-def product_list_api(request):
-    products = Product.objects.all()[:20] #list
-    data = ProductSerializer(products,many=True,context={'request':request}).data #json
-    return Response({'products':data})
+# @api_view(['GET'])
+# def product_list_api(request):
+#     products = Product.objects.all()[:20] #list
+#     data = ProductSerializer(products,many=True,context={'request':request}).data #json
+#     return Response({'products':data})
 
 
 
 
-@api_view(['GET'])
-def product_detail_api(request,product_id):
-    products = Product.objects.get(id=product_id)  #list
-    data = ProductSerializer(products,context={'request':request}).data #json
-    return Response({'product':data})
+# @api_view(['GET'])
+# def product_detail_api(request,product_id):
+#     products = Product.objects.get(id=product_id)  #list
+#     data = ProductSerializer(products,context={'request':request}).data #json
+#     return Response({'product':data})
 
 
 
@@ -30,3 +30,12 @@ class ProductListPI(generics.ListCreateAPIView):
 class ProductDetailAPI(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class BrandListAPI(generics.ListAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+    
+class BrandDetailAPI(generics.RetrieveAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ProductList , ProductDetail ,BrandList,BrandDetail,queryset_debug
-from .api import product_list_api, product_detail_api , ProductListPI, ProductDetailAPI
+from .api import  ProductListPI, ProductDetailAPI , BrandDetailAPI , BrandListAPI
 urlpatterns = [
     path('', ProductList.as_view()),
     path('debug', queryset_debug),
@@ -12,4 +12,9 @@ urlpatterns = [
     #api
     path('api/list', ProductListPI.as_view()),
     path('api/list/<int:pk>', ProductDetailAPI.as_view()),
+
+
+
+    path('brands/api/list', BrandListAPI.as_view()),
+    path('brands/api/list/<int:pk>', BrandDetailAPI.as_view()),
 ]

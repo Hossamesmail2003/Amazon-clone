0120d23fb258f9fc760871385478ed0515c8ca66
Hossamesmail2003 <hossam02003@gmail.com>
2024-10-25 22:40:07 +0300

    add dj_rest_auth library

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
+    'dj_rest_auth',
     'product',
     'settings',
 ]
@@ -152,3 +153,8 @@ MEDIA_ROOT= BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_AUTH = {
+    'USE_JWT': True,
+    'JWT_AUTH_COOKIE': 'jwt-auth',
+}

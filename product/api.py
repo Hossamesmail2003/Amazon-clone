7f15c483350d1bbce1e11cc010f562f19ff051d8
Hossamesmail2003 <hossam02003@gmail.com>
2024-10-25 22:25:14 +0300

    add jwt token

--- product/api.py
+++ product/api.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import filters
 from .myfilter import ProductFilter
 from .mypagination import MyPagination
@@ -36,6 +37,7 @@ class ProductListPI(generics.ListCreateAPIView):
     ordering_fields = ['price', 'quantity']
     filterset_class = ProductFilter
     pagination_class = MyPagination
+    permission_classes = [IsAuthenticated]
 
 class ProductDetailAPI(generics.RetrieveAPIView):
     queryset = Product.objects.all()
--- project/settings.py
+++ project/settings.py
@@ -41,11 +41,16 @@ INSTALLED_APPS = [
     "debug_toolbar",
     'rest_framework',
     'django_filters',
+    'rest_framework_simplejwt',
+    'rest_framework.authtoken',
     'product',
     'settings',
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 30,

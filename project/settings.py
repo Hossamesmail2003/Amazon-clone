2e32443356293b85885924237eec39c4a67ad3e5
Hossamesmail2003 <hossam02003@gmail.com>
2024-11-01 18:18:33 +0200

    add translation to template

--- project/settings.py
+++ project/settings.py
@@ -68,6 +68,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.locale.LocaleMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
@@ -139,7 +140,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
@@ -168,3 +169,8 @@ REST_AUTH = {
     'JWT_AUTH_COOKIE': 'jwt-auth',
 }
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+LOCALE_PATHS = ['locale']
+LANGUAGES = [
+    ("ar", ("Arabic")),
+    ("en", ("English")),
+]
--- project/urls.py
+++ project/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('', include('settings.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path("i18n/", include("django.conf.urls.i18n")),
 ] + debug_toolbar_urls()
 
 urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

4e03c30a804629194a2d41abebbbe850748384cb
Hossamesmail2003 <hossam02003@gmail.com>
2024-11-05 19:47:34 +0200

    add orderlist api

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import OrderList , checkout ,add_to_cart ,remove_from_cart
-from .api import CartDetailCreatAPI
+from .api import CartDetailCreatAPI,OrderListAPI
 app_name = 'orders'
 
 urlpatterns = [
@@ -13,5 +13,6 @@ urlpatterns = [
 
 
     #api
-    path('api/<str:username>/cart',CartDetailCreatAPI.as_view())
+    path('api/<str:username>/cart',CartDetailCreatAPI.as_view()),
+    path('api/list/<str:username>',OrderListAPI.as_view()),
 ]
